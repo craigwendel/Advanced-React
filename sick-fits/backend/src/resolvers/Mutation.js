4e0feb7aa1d59da0c833d46ed43795514cc9d815
Craig Wendel <wendelcraig@gmail.com>
2018-11-20 12:19:38 -0600

    Add additional user properties in the schema for user signup.  Add mutation to sign up a new user, hash the password, and pass a jwt to the browser.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         const item = await ctx.db.mutation.createItem({
@@ -20,10 +23,35 @@ const Mutations = {
     },
     async deleteItem(parent, args, ctx, info) {
         const where = { id: args.id };
+        // 1. Find the item
         const item = await ctx.db.query.item({ where }, `{ id title}`);
-
+        // 2. Check if they own the item or have permissions
+        // TODO
+        // 3. Delete the item
         return ctx.db.mutation.deleteItem({ where }, info);
-    }
+    },
+    async signup(parent, args, ctx, info) {
+        args.email = args.email.toLowerCase();
+        // hash the user password
+        const password = await bcrypt.hash(args.password, 10);
+        // create the user
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: { set: ['USER'] },
+            },
+        }, info);
+        // create JWT Token
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // set jwt as cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie 
+        });
+        return user;
+    },
+
 };
 
 module.exports = Mutations;
